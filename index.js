e1c5586c0dc0d20e1c65e86af87dfcfdb04a3110
Srijeet Patil <srijeetpatil7@gmail.com>
2022-03-25 19:59:43 +0530

    First commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const Gun = require('gun');
-const app = express()
-const port = 3030
+const express = require("express");
+const Gun = require("gun");
+const app = express();
+const port = process.env.PORT || 3030;
 app.use(Gun.serve);
 
 const server = app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);  
+});
 
 Gun({ web: server });
